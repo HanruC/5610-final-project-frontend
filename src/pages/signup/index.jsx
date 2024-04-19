9c15ad8ea294b508714f927632c0737588a14b76
HanruC <chen.hanru@northeastern.edu>
2024-04-19 22:14:59 +0800

    fix show error

--- src/pages/signup/index.jsx
+++ src/pages/signup/index.jsx
@@ -14,6 +14,7 @@ const Signup = () => {
             navigate("/login");
         } catch (err) {
             console.log(err);
+            message.error("Fail to register!");
         }
     };
 
